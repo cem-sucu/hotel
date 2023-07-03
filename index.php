9a92e0b3e063dbdb4455e203853b38c9c50e6b69
cem-sucu <cems.cems.17@gmail.com>
2023-07-03 16:56:52 +0200

    ajout

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ spl_autoload_register(function ($class_name){
     require 'classes/'. $class_name . '.php';
 });
 
+// cree hotel
+$hotel1 = new Hotel("Hilton **** Strasbourg", " 10 rue de la Gare ", 30);
 
 
-
+// affciher hotel
+$hotel1->afficherInfos();
 
 ?>
